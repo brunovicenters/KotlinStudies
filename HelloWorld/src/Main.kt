6a342e8e077153b2671a0cf17cf690a77e8cc3b9
bruno <brunovicente.dev@gmail.com>
2024-06-20 19:03:57 -0300

    First commit

---
+++ HelloWorld/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // Print text in console
+    println("Hello World!")
+
+    //constant
+    val name: String = "Bruno Vicente"
+
+    // variable
+    var age: Int = 19
+
+    println(imprimirSoma(age, 8))
+}
+
+// Declaring Functions
+fun imprimirSoma(a: Int, b: Int): Int {
+    return a + b;
+}
