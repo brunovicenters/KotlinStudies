155d2be59343281cf92b02984bccfa804c440532
bruno <brunovicente.dev@gmail.com>
2024-06-23 15:11:32 -0300

    Added loop, arrays and template strings

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -3,12 +3,25 @@ fun main() {
     println("Hello World!")
 
     //constant
-    val name: String = "Bruno Vicente"
+    val myName: String = "Bruno Vicente"
 
     // variable
     var age: Int = 19
 
     println(imprimirSoma(age, 8))
+
+    // Array
+    val names = arrayOf("Bruno", "Vicente", "Tarsila", "Reis")
+    println(names[1])
+
+    println("\n**************\nThe names are:")
+    // Loop
+    for (name in names) {
+        println(name)
+    }
+
+    // Template Strings
+    println("\nMy name is $myName and I am $age years old, ${if (age > 18) "and I can vote" else "and I can't vote"}")
 }
 
 // Declaring Functions
