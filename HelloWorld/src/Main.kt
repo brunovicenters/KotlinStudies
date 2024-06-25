ab48237f861e5028a8feba48545a4542eee98b8b
bruno <brunovicente.dev@gmail.com>
2024-06-24 22:48:34 -0300

    first() and last()

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -11,17 +11,20 @@ fun main() {
     println(imprimirSoma(age, 8))
 
     // Array
-    val names = arrayOf("Bruno", "Vicente", "Tarsila", "Reis")
+    val names = listOf("Bruno", "Vicente", "Tarsila", "Reis")
     println(names[1])
 
     println("\n**************\nThe names are:")
-    // Loop
+
+    // For Loop
     for (name in names) {
         println(name)
     }
 
     // Template Strings
     println("\nMy name is $myName and I am $age years old, ${if (age > 18) "and I can vote" else "and I can't vote"}")
+
+    println("\nFirst letter of my name is ${myName.first()}, and the last letter is ${myName.last()}")
 }
 
 // Declaring Functions
