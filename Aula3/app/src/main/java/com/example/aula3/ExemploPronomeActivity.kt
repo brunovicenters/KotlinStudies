0ff700db410b93acf5ce57bf3f090cec5cdd374c
bruno <brunovicente.dev@gmail.com>
2024-07-29 17:47:55 -0300

    Created pronome example and started preferencia contrato

---
+++ Aula3/app/src/main/java/com/example/aula3/ExemploPronomeActivity.kt
@@ -0,0 +1,42 @@
+package com.example.aula3
+
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+
+class ExemploPronomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exemplo_pronome)
+
+        val editNome = findViewById<EditText>(R.id.editNome)
+        val groupPronomes = findViewById<RadioGroup>(R.id.groupPronomes)
+        val btnCadastrar = findViewById<Button>(R.id.btnCadastrar)
+
+
+        btnCadastrar.setOnClickListener {
+            val nome = editNome.text.toString()
+            var pronome: String = when (groupPronomes.checkedRadioButtonId) {
+                R.id.radioEle -> "Ele/Dele"
+                R.id.radioEla -> "Ela/Dela"
+                R.id.radioNeutro -> {
+                    "Elu/Delu"
+                }
+                else -> "You didn't choose anything"
+            }
+            alert("Cadastrado com sucesso!", "Olá, $nome, você escolheu os pronomes $pronome")
+        }
+    }
+
+    fun alert(title: String, msg: String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(msg)
+            .setPositiveButton("OK", null)
+            .create()
+            .show()
+    }
+}
