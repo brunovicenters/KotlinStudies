e43e3ba3c668f86c5c469f443b0f0f7d03533346
bruno <brunovicente.dev@gmail.com>
2024-07-31 09:40:08 -0300

    Added Time picker

--- Aula3/app/src/main/java/com/example/aula3/DateTimeActivity.kt
+++ Aula3/app/src/main/java/com/example/aula3/DateTimeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.aula3
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aula3.databinding.ActivityDateTimeBinding
+import java.sql.Time
 
 class DateTimeActivity : AppCompatActivity() {
 
@@ -21,18 +23,34 @@ class DateTimeActivity : AppCompatActivity() {
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR)
+        val minute = calendar.get(Calendar.MINUTE)
 
         binding.imgDate.setOnClickListener {
             DatePickerDialog(this, { calView, sYear, sMonth, sDay ->
                 binding.etDate.setText("$sDay/${sMonth + 1}/$sYear")
-
             }, year, month, day).show()
         }
 
-        binding.btnSet.setOnClickListener {
+        binding.imgTime.setOnClickListener {
+
+            TimePickerDialog(this, { timeView, sHour, sMinute ->
+                val tdHour = sHour.toString().padStart(2, '0')
+                val tdMinutes = sMinute.toString().padStart(2, '0')
+                binding.etTime.setText("$tdHour:$tdMinutes")
+            }, hour, minute, true).show()
 
         }
 
+        binding.btnSet.setOnClickListener {
+            val msg = """
+                |Data: ${binding.etDate.text}
+                |
+                |Hora: ${binding.etTime.text}
+                """.trimMargin()
+
+            alert("Data e Hora", msg, this)
+        }
 
     }
 }
