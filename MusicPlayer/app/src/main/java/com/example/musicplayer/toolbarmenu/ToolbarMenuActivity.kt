3f77cd5960beb65f174abb8bb24005e7e6714dd9
bruno <brunovicente.dev@gmail.com>
2024-08-05 21:24:42 -0300

    Added toolbar menu

--- MusicPlayer/app/src/main/java/com/example/musicplayer/toolbarmenu/ToolbarMenuActivity.kt
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/toolbarmenu/ToolbarMenuActivity.kt
@@ -1,12 +1,29 @@
 package com.example.musicplayer.toolbarmenu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.musicplayer.R
 
 class ToolbarMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar_menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbarmenu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.recentes -> startActivity(Intent(this, OtherActivity::class.java))
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
